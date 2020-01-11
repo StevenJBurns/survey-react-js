656828f83ca770f4fcf41ff3b0fcdfa8bedd25ba
Steven J Burns <stevenjburns@msn.com>
2020-01-10 22:46:45 -0700

    chore: rename react-root in public HTML doc; rename index and App files to JSX and SCSS externsions as required

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactLogo from './assets/react-logo.svg';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={ReactLogo} className="App-logo" alt="React Logo" />
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +19,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
